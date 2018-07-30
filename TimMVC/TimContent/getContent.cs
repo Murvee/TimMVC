edd2032360ce9333efe7e3e8e8b25c1b20de4081
Timothy Zonoe <tzonoe23@gmail.com>
2018-07-30 09:35:20 +0100

    Importing Project

---
+++ TimMVC/TimContent/getContent.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TimMVC.TimContent
+{
+    public class getContent
+    {
+        //This will get content
+    }
+}
